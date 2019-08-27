9783f1360bd539339250cb444b37dbba0a2999a2
Daniel Pickett <daniel@danielpickett.me>
2019-08-27 17:04:04 -0400

    Set color pickers to showAlways: true

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-import pickrConfig from "./pickrConfig.js";
+// import pickrConfig from "./pickrConfig.js";
 
-// var output = chromaResult.map(result => {
-//   console.log(result)
-//   return `<div style="background-color: ${result}; height: 1em"></div>`
-// }).join('')
+const pickrConfig = {
+  theme: 'classic', // 'classic', or 'monolith', or 'nano'
+  inline: true,
+  useAsButton: true,
+  appClass: "color-picker-app",
+  showAlways: true,
+  components: {
+    hue: true,
+    interaction: {
+      hex: true,
+      rgba: true,
+      hsla: true,
+      hsva: true,
+      input: true,
+      clear: true,
+      save: true
+    }
+  }
+};
 
 let colorStart = '#F5F8FA';
 let colorEnd = '#152633';
